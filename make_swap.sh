f4f8405eb3df30b08ca21b51be8010e63b893118
zersonix <43750953+zersonix@users.noreply.github.com>
2018-10-03 08:45:44 +0300

    Create make_swap.sh

---
+++ make_swap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo fallocate -l 20G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
